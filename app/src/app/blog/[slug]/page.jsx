19eef90126dd85bd147b5766e9e2323d171a5f7b
tiffany <ting11222001@gmail.com>
2024-02-24 17:48:01 +1030

    Create layout for single post page.
    - fix footer on mobile screen.
    - update hamburger menu.

--- app/src/app/blog/[slug]/page.jsx
+++ app/src/app/blog/[slug]/page.jsx
@@ -1,6 +1,34 @@
+import Image from "next/image"
+import styles from "./singlePost.module.css"
+
 const SinglePostPage = () => {
   return (
-    <div>SinglePostPage</div>
+    <div className={styles.container}>
+      {/* left */}
+      <div className={styles.imgContainer}>
+        <Image src="https://images.unsplash.com/photo-1618677366787-9727aacca7ea?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" fill className={styles.img} />
+      </div>
+      {/* right */}
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image src="https://images.unsplash.com/photo-1618677366787-9727aacca7ea?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt=""  className={styles.avatar} width={50} height={50} />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>
+              Published
+            </span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Terry Je</span>
+            <span className={styles.detailValue}>
+              2024.01.01
+            </span>
+          </div>
+        </div>
+        <div className={styles.content}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, ut placeat illo doloremque magnam ipsum laboriosam necessitatibus distinctio culpa aliquid tenetur id a? Ratione eveniet incidunt adipisci fugit architecto commodi.</div>
+      </div>
+    </div>
   )
 }
 
