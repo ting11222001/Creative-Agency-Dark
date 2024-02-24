7e9059915f1c3005bd56b4ca0c6442774049f3be
tiffany <ting11222001@gmail.com>
2024-02-24 14:48:17 +1030

    Add home page layout with images.

--- app/src/app/page.jsx
+++ app/src/app/page.jsx
@@ -1,5 +1,32 @@
+import styles from "./home.module.css"
+import Image from "next/image"
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+  <div className={styles.container}>
+      {/* left */}
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts Agency.</h1>
+        <p className={styles.desc}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero
+          blanditiis adipisci minima reiciendis a autem assumenda dolore.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          {/* "fill" according to the div */}
+          <Image src="/brands.png" alt="" fill className={styles.brandImg}/>
+        </div>
+      </div>      
+      {/* right */}
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg}/>
+      </div>
+    </div>
+
+  );
 };
 
 export default HomePage;
--- app/src/components/navbar/Navbar.jsx
+++ app/src/components/navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Links from '@/components/navbar/links/Links'
 import styles from "../navbar/navbar.module.css"
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>Logo</div>
+      <Link href="/" className={styles.logo}>Logo</Link>
       <div>
         <Links />
       </div>
