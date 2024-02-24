d960238ef170c516767f64695265ef871dce898a
tiffany <ting11222001@gmail.com>
2024-02-24 14:14:07 +1030

    Add hidden hamburger.

--- app/src/components/navbar/links/Links.jsx
+++ app/src/components/navbar/links/Links.jsx
@@ -1,47 +1,66 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 import styles from './links.module.css'
 import NavLink from './navLink/NavLink'
 
+const links = [
+  {
+    title: "Homepage",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  }
+];
+
 const Links = () => {
-  const links = [
-    {
-      title: "Homepage",
-      path: "/",
-    },
-    {
-      title: "About",
-      path: "/about",
-    },
-    {
-      title: "Contact",
-      path: "/contact",
-    },
-    {
-      title: "Blog",
-      path: "/blog",
-    }
-  ];
+  const [open, setOpen] = useState(false);
+
 
   // TEMPORARY
   const session = true;
   const isAdmin = true;
 
   return (
-    <div className={styles.links}>
-      {links.map((link => (
-        <NavLink item={link} key={link.title}></NavLink>
-      )))}
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link => (
+          <NavLink item={link} key={link.title}></NavLink>
+        )))}
+        {
+          session ? (
+            <>
+              {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+              <button className={styles.logout}>Logout</button>
+            </>
+          ) : (
+            <>
+              <NavLink item={{ title: "Login", path: "/login" }}></NavLink>
+            </>
+          )
+        }
+      </div>
+      <button onClick={() => setOpen((prev) => !prev)} className={styles.menuButton}>Menu</button>
       {
-        session ? (
-          <>
-            {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
-            <button className={styles.logout}>Logout</button>
-          </>
-        ) : (
-          <>
-            <NavLink item={{ title: "Login", path: "/login" }}></NavLink>
-          </>
-        )
+        open && <div className={styles.mobileLinks}>
+          {
+            links.map(
+              (link) => (
+                <NavLink item={link} key={link.title}></NavLink>
+              )
+            )
+          }
+        </div>
       }
     </div>
   )
