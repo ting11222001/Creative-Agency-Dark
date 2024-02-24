402c72f0ed071c33d1c833d256cb6e8f669a7368
tiffany <ting11222001@gmail.com>
2024-02-24 21:56:50 +1030

    Try data fetching from json fake API.

--- app/src/app/blog/[slug]/page.jsx
+++ app/src/app/blog/[slug]/page.jsx
@@ -1,7 +1,24 @@
 import Image from "next/image"
 import styles from "./singlePost.module.css"
 
-const SinglePostPage = () => {
+const getData = async (slug) => {
+
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`, { cache: "no-store" });
+  // fetch by default will cache the data
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch post!");
+  }
+
+  return res.json();
+}
+
+const SinglePostPage = async ({ params }) => {
+
+  // console.log(params);
+
+  const post = await getData(params.slug);
+
   return (
     <div className={styles.container}>
       {/* left */}
@@ -10,9 +27,9 @@ const SinglePostPage = () => {
       </div>
       {/* right */}
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>Title</h1>
+        <h1 className={styles.title}>{post.title}</h1>
         <div className={styles.detail}>
-          <Image src="https://images.unsplash.com/photo-1618677366787-9727aacca7ea?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt=""  className={styles.avatar} width={50} height={50} />
+          <Image src="https://images.unsplash.com/photo-1618677366787-9727aacca7ea?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className={styles.avatar} width={50} height={50} />
           <div className={styles.detailText}>
             <span className={styles.detailTitle}>Author</span>
             <span className={styles.detailValue}>
@@ -26,7 +43,7 @@ const SinglePostPage = () => {
             </span>
           </div>
         </div>
-        <div className={styles.content}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, ut placeat illo doloremque magnam ipsum laboriosam necessitatibus distinctio culpa aliquid tenetur id a? Ratione eveniet incidunt adipisci fugit architecto commodi.</div>
+        <div className={styles.content}>{post.body}</div>
       </div>
     </div>
   )
--- app/src/app/blog/page.jsx
+++ app/src/app/blog/page.jsx
@@ -1,21 +1,29 @@
 import PostCard from "@/components/postCard/PostCard"
 import styles from "./blog.module.css"
 
-const BlogPage = () => {
+const getData = async () => {
+
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", { next: { revalidate: 3600 } });
+  // fetch by default will cache the data
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch posts!");
+  }
+
+  return res.json();
+}
+
+const BlogPage = async () => {
+
+  const posts = await getData();
+
   return (
     <div className={styles.container}>
-      <div className={styles.post}>
-        <PostCard></PostCard>
-      </div>
-      <div className={styles.post}>
-        <PostCard></PostCard>
-      </div>
-      <div className={styles.post}>
-        <PostCard></PostCard>
-      </div>
-      <div className={styles.post}>
-        <PostCard></PostCard>
-      </div>
+      {posts.map((post) => (
+        <div className={styles.post} key={post.id}>
+          <PostCard post={post} />
+        </div>
+      ))}
     </div>
   )
 }
