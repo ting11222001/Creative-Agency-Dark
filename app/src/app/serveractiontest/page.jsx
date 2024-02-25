260d981373fb3456a3571b4997c3a0c2af11d913
tiffany <ting11222001@gmail.com>
2024-02-25 20:51:47 +1030

    I can also create action inside a page component to use Server Action.

--- app/src/app/serveractiontest/page.jsx
+++ app/src/app/serveractiontest/page.jsx
@@ -2,9 +2,14 @@ import { sayHello } from '@/lib/action'
 import React from 'react'
 
 const ServerActionTestPage = () => {
+
+  const actionInComponent = async () => {
+    "use server"
+    console.log("it works!")
+  }
   return (
     <div>
-      <form action={sayHello}>
+      <form action={actionInComponent}>
         <button>Test me</button>
       </form>
     </div>
