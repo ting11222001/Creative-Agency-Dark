44ae71522f67ad4efd8e59a21ed05fbd9523932c
tiffany <ting11222001@gmail.com>
2024-02-25 14:41:35 +1030

    Connect with MongoDB and use the data inside MongoDB database.
    - note that in .env, the collection's name i.e. agency needs to be included.

--- app/src/lib/models.js
+++ app/src/lib/models.js
@@ -54,5 +54,6 @@ const postSchema = new mongoose.Schema({
   },
 }, { timestamps: true });
 
-export const User = mongoose.models.User || mongoose.model("User", userSchema);
-export const Post = mongoose.models.Post || mongoose.model("Post", postSchema);
+// mongoose.models: An array containing all models associated with this Mongoose instance.
+export const User = mongoose.models?.User || mongoose.model("User", userSchema);
+export const Post = mongoose.models?.Post || mongoose.model("Post", postSchema);
