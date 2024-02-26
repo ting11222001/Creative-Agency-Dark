c889d574eaca9e301ea75ac68c9365405088d100
tiffany <ting11222001@gmail.com>
2024-02-26 17:02:20 +1030

    Create a loading spinner when switching routes.

--- app/src/app/loading.jsx
+++ app/src/app/loading.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import styles from './loading.module.css'
 
 const Loading = () => {
   return (
-    <div>Loading</div>
+    <div className={styles.container}>
+      <div className={styles.spinner}></div>
+    </div>
   )
 }
 
